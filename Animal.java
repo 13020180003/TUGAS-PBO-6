c941112a19c7bd7d5c01aace4ba2fbbf595906d8
13020180003 <61999270+13020180003@users.noreply.github.com>
2020-04-21 04:24:28 -1200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,14 @@
+Jawaban Soal NO.6
+public class Animal {
+public String nama;
+public int umur;
+    public void makan()
+    {	
+        System.out.println("Sedang Makan");
+    }
+
+    public void mengeluarkanSuara()
+    {
+        System.out.println("Sedang Bersuara");
+    }
+}
